d0d79fe211df52c32995475f5ba9a7232352ba36
maeste <stefano.maestri@ironjacamar.org>
2017-11-07 23:34:00 +0100

    usingEthAccounts

--- contracts/SmartNotary.sol
+++ contracts/SmartNotary.sol
@@ -5,23 +5,23 @@ contract SmartNotary
 {
     struct FileDetails {
         uint timestamp;
-        string owner;
+        address owner;
     }
     
     mapping (string => FileDetails) files;
     
-    event logFileAddedStatus(bool status, uint timestamp, string owner, string fileHash);
+    event logFileAddedStatus(bool status, uint timestamp, address owner, string fileHash);
     
     //this is used to store the owner of file at the block
-    function set(string owner, string fileHash)
+    function set(string fileHash)
     {
         require(files[fileHash].timestamp == 0);
-        files[fileHash] = FileDetails(block.timestamp, owner);
-        logFileAddedStatus(true, block.timestamp, owner, fileHash);
-        
+        files[fileHash] = FileDetails(block.timestamp, msg.sender);
+        logFileAddedStatus(true, block.timestamp, msg.sender, fileHash);
+
     }
     //this is used to get file information
-    function get(string fileHash) constant returns (uint timestamp, string owner){
+    function get(string fileHash) constant returns (uint timestamp, address owner){
         return (files[fileHash].timestamp, files[fileHash].owner);
     }
 }
