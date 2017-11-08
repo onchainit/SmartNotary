6a8c5a521724ee5c0163630247385f4f46634e48
maeste <stefano.maestri@ironjacamar.org>
2017-11-08 21:12:00 +0100

    PayForNotarization

--- contracts/SmartNotary.sol
+++ contracts/SmartNotary.sol
@@ -7,15 +7,24 @@ contract SmartNotary
         uint timestamp;
         address owner;
     }
+
+    address notaryOwner = msg.sender;
     
     mapping (string => FileDetails) files;
     
     event logFileAddedStatus(bool status, uint timestamp, address owner, string fileHash);
-    
+
+    modifier onlyBy(address _account)
+    {
+        require(msg.sender == _account);
+        _;
+    }
+
     //this is used to store the owner of file at the block
-    function set(string fileHash)
+    function set(string fileHash) payable
     {
         require(files[fileHash].timestamp == 0);
+        require(msg.value >= 0.1 ether );
         files[fileHash] = FileDetails(block.timestamp, msg.sender);
         logFileAddedStatus(true, block.timestamp, msg.sender, fileHash);
 
@@ -24,4 +33,10 @@ contract SmartNotary
     function get(string fileHash) constant returns (uint timestamp, address owner){
         return (files[fileHash].timestamp, files[fileHash].owner);
     }
+
+    function withdraw() onlyBy(notaryOwner) {
+        msg.sender.transfer(this.balance);
+    }
+
+
 }
